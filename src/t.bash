c4185a09620499926e68e107aaec44f8d9dc8349
Darius <darius@adroiti.com>
2018-08-29 09:40:24 +0300

    update bash file

--- src/t.bash
+++ src/t.bash
@@ -1,4 +1,5 @@
 #!/bin/unknown
-# should not report SC2140 
-# should report SC1008
+# should not report SC1008, SC2140 
+# should report SC1098
 echo "<img src="foo.png" />" > file.html
+eval $var=(a b)
