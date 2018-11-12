091c1dd55b1b1630c1291757ab67246985079f95
cordis-dev <darius@adroiti.com>
2018-11-12 14:21:47 +0200

    Update t.cs

--- src/t.cs
+++ src/t.cs
@@ -1,5 +1,16 @@
 public class Test
 {
+	public bool Enabled
+    {
+		
+        get
+
+        {
+
+            return this.enabled;
+        }
+    }
+	
 	public virtual ActionResult Claim(PageContext contex)
 	{
 		if (context.CanBeClaimed)
